166e637db5e3442a231ff8b342bebcca4c777094
Tim Lohrer <me@timlohrer.de>
2024-01-27 10:30:32 +0100

    feat(backend): add TokenManager

--- src-tauri/src/app/app_data.rs
+++ src-tauri/src/app/app_data.rs
@@ -4,6 +4,7 @@ use std::path::PathBuf;
 use anyhow::Result;
 use serde::{Deserialize, Serialize};
 use tokio::fs;
+use keyring::Entry as KeyringEntry;
 
 use crate::app::api::LoginData;
 use crate::LAUNCHER_DIRECTORY;
@@ -59,6 +60,70 @@ impl LauncherOptions {
             return LAUNCHER_DIRECTORY.data_dir().to_path_buf();
         }
         PathBuf::from(&self.data_path)
+    }
+}
+
+pub struct TokenManager {}
+
+impl TokenManager {
+    const SERVICE: &'static str = "noriskclient-launcher";
+
+    pub fn load_tokens(&self, mut login_data: LoginData) -> LoginData {
+        // logic for loading tokens
+        let uuid = login_data.uuid.clone();
+        let keyring_mc_token = KeyringEntry::new(Self::SERVICE, &*format!("{}-{}", uuid, "mcToken")).map_err(|e| format!("Failed to fetch keyring-mc-token: {}", e.to_string())).unwrap();
+        let keyring_access_token = KeyringEntry::new(Self::SERVICE, &*format!("{}-{}", uuid, "accessToken")).map_err(|e| format!("Failed to fetch keyring-access-token: {}", e.to_string())).unwrap();
+        let keyring_refresh_token = KeyringEntry::new(Self::SERVICE, &*format!("{}-{}", uuid, "refreshToken")).map_err(|e| format!("Failed to fetch keyring-refresh-token: {}", e.to_string())).unwrap();
+        let keyring_norisk_token = KeyringEntry::new(Self::SERVICE, &*format!("{}-{}", uuid, "noriskToken")).map_err(|e| format!("Failed to fetch keyring-norisk-token: {}", e.to_string())).unwrap();
+        let keyring_experimental_token = KeyringEntry::new(Self::SERVICE, &*format!("{}-{}", uuid, "experimentalToken")).map_err(|e| format!("Failed to fetch keyring-experimental-token: {}", e.to_string())).unwrap();
+
+        login_data.mc_token = keyring_mc_token.get_password().unwrap();
+        login_data.access_token = keyring_access_token.get_password().unwrap();
+        login_data.refresh_token = keyring_refresh_token.get_password().unwrap();
+        login_data.norisk_token = keyring_norisk_token.get_password().unwrap();
+        login_data.experimental_token = Some(keyring_experimental_token.get_password().unwrap());
+
+        return login_data;
+    }
+
+    pub fn store_tokens(&mut self, mut login_data: LoginData) -> LoginData {
+        // logic for storing tokens
+        let uuid = login_data.uuid.clone();
+        let keyring_mc_token = KeyringEntry::new(Self::SERVICE, &*format!("{}-{}", uuid, "mcToken")).map_err(|e| format!("Failed to fetch keyring-mc-token: {}", e.to_string())).unwrap();
+        let keyring_access_token = KeyringEntry::new(Self::SERVICE, &*format!("{}-{}", uuid, "accessToken")).map_err(|e| format!("Failed to fetch keyring-access-token: {}", e.to_string())).unwrap();
+        let keyring_refresh_token = KeyringEntry::new(Self::SERVICE, &*format!("{}-{}", uuid, "refreshToken")).map_err(|e| format!("Failed to fetch keyring-refresh-token: {}", e.to_string())).unwrap();
+        let keyring_norisk_token = KeyringEntry::new(Self::SERVICE, &*format!("{}-{}", uuid, "noriskToken")).map_err(|e| format!("Failed to fetch keyring-norisk-token: {}", e.to_string())).unwrap();
+        let keyring_experimental_token = KeyringEntry::new(Self::SERVICE, &*format!("{}-{}", uuid, "experimentalToken")).map_err(|e| format!("Failed to fetch keyring-experimental-token: {}", e.to_string())).unwrap();
+
+        keyring_mc_token.set_password(&login_data.mc_token).unwrap();
+        keyring_access_token.set_password(&login_data.access_token).unwrap();
+        keyring_refresh_token.set_password(&login_data.refresh_token).unwrap();
+        keyring_norisk_token.set_password(&login_data.norisk_token).unwrap();
+        keyring_experimental_token.set_password(&login_data.experimental_token.unwrap()).unwrap();
+
+        login_data.mc_token = String::new();
+        login_data.access_token = String::new();
+        login_data.refresh_token = String::new();
+        login_data.norisk_token = String::new();
+        login_data.experimental_token = Some(String::new());
+
+        return login_data
+    }
+
+    pub fn delete_tokens(&mut self, login_data: LoginData) {
+        // logic for deleting tokens
+        let uuid = login_data.uuid.clone();
+        let keyring_mc_token = KeyringEntry::new(Self::SERVICE, &*format!("{}-{}", uuid, "mcToken")).map_err(|e| format!("Failed to fetch keyring-mc-token: {}", e.to_string())).unwrap();
+        let keyring_access_token = KeyringEntry::new(Self::SERVICE, &*format!("{}-{}", uuid, "accessToken")).map_err(|e| format!("Failed to fetch keyring-access-token: {}", e.to_string())).unwrap();
+        let keyring_refresh_token = KeyringEntry::new(Self::SERVICE, &*format!("{}-{}", uuid, "refreshToken")).map_err(|e| format!("Failed to fetch keyring-refresh-token: {}", e.to_string())).unwrap();
+        let keyring_norisk_token = KeyringEntry::new(Self::SERVICE, &*format!("{}-{}", uuid, "noriskToken")).map_err(|e| format!("Failed to fetch keyring-norisk-token: {}", e.to_string())).unwrap();
+        let keyring_experimental_token = KeyringEntry::new(Self::SERVICE, &*format!("{}-{}", uuid, "experimentalToken")).map_err(|e| format!("Failed to fetch keyring-experimental-token: {}", e.to_string())).unwrap();
+
+        keyring_mc_token.delete_password();
+        keyring_access_token.delete_password();
+        keyring_refresh_token.delete_password();
+        keyring_norisk_token.delete_password();
+        keyring_experimental_token.delete_password();
     }
 }
 
