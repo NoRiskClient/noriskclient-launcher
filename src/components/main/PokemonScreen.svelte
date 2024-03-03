598ce35c5336cb10e07d7e2bac076e94ab43e577
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2024-03-03 10:17:58 +0100

    bump to 0.4.0

--- src/components/main/PokemonScreen.svelte
+++ src/components/main/PokemonScreen.svelte
@@ -401,7 +401,7 @@
     <SkinButton on:launch={runClient} on:requestBranches={requestBranches} bind:options={options}></SkinButton>
     <div transition:scale={{ x: 15, duration: 300, easing: quintOut }} on:selectstart={preventSelection}
         on:mousedown={preventSelection} class="copyright">
-      © 2000-{new Date().getFullYear()} HGLabor/Friends Inc. v0.3.9
+      © 2000-{new Date().getFullYear()} HGLabor/Friends Inc. v0.4.0
     </div>
   {/if}
 </div>
