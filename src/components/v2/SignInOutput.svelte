4b642eec57afa3c6c2fcbd629805e4059693bb44
eazyPaul <eazypaulmail@gmail.com>
2024-06-27 15:08:18 +0200

    Add shimmer effect (IDK why it's kinda blurry, but I don't have a fix)

--- src/components/v2/SignInOutput.svelte
+++ src/components/v2/SignInOutput.svelte
@@ -17,10 +17,12 @@
   });
 </script>
 
-<h1 class="branch-font" style="position:absolute"
+<h1 class="branch-font shimmer" style="position:absolute"
     transition:scale={{ x: 15, duration: 300, easing: quintOut }}>{microsoftOutput}</h1>
 
 <style>
+    /* Shimmer animation credits to https://codepen.io/joshuapekera/pen/xGjMMq (https://codepen.io/joshuapekera) */
+    /* TODO: Fix text blurriness (ChatGPT says its hard to fix) */
     .branch-font {
         font-family: 'Press Start 2P', serif;
         font-size: 18px;
@@ -28,5 +30,63 @@
         color: var(--primary-color);
         text-shadow: 2px 2px var(--primary-color-text-shadow);
         cursor: default;
+    }
+
+    .shimmer {
+        text-align: center;
+        color: rgba(255, 255, 255, 0.1);
+        background: linear-gradient(90deg, #222 25%, #fff 50%, #222 75%);
+        background-size: 300% 100%;
+        -webkit-background-clip: text;
+        -moz-background-clip: text;
+        background-clip: text;
+        -webkit-animation-name: shimmer;
+        -moz-animation-name: shimmer;
+        animation-name: shimmer;
+        -webkit-animation-duration: 5s;
+        -moz-animation-duration: 5s;
+        animation-duration: 5s;
+        -webkit-animation-iteration-count: infinite;
+        -moz-animation-iteration-count: infinite;
+        animation-iteration-count: infinite;
+        background-repeat: no-repeat;
+        background-position: 0 0;
+        background-color: #222;
+    }
+
+    @-moz-keyframes shimmer {
+        0% {
+            background-position: 100% 0;
+        }
+        100% {
+            background-position: -100% 0;
+        }
+    }
+
+    @-webkit-keyframes shimmer {
+        0% {
+            background-position: 100% 0;
+        }
+        100% {
+            background-position: -100% 0;
+        }
+    }
+
+    @-o-keyframes shimmer {
+        0% {
+            background-position: 100% 0;
+        }
+        100% {
+            background-position: -100% 0;
+        }
+    }
+
+    @keyframes shimmer {
+        0% {
+            background-position: 100% 0;
+        }
+        100% {
+            background-position: -100% 0;
+        }
     }
 </style>
