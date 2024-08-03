7e6f75079865f601a11a37a3cd62619ae9e795ad
Tim Lohrer <me@timlohrer.de>
2024-08-03 04:44:09 +0200

    https://github.com/NoRiskClient/noriskclient-launcher/pull/52#discussion_r1701821760

--- src/components/v2/SignInOutput.svelte
+++ src/components/v2/SignInOutput.svelte
@@ -4,7 +4,7 @@
   import { quintOut } from "svelte/easing";
   import { scale } from "svelte/transition";
 
-  let microsoftOutput = "SIGN IN";
+  let microsoftOutput = "PRESS START";
   let dots = "";
   let microsoftFlag = false;
 
