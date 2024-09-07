a58d1a25fc902052eae06125a751d9853671eec5
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2024-09-07 10:06:26 +0200

    refactor: sorry tim ich weiß du haste einen großen :airplane:

--- src-tauri/src/app/gui.rs
+++ src-tauri/src/app/gui.rs
@@ -817,9 +817,6 @@ async fn discord_auth_link(options: LauncherOptions, credentials: Credentials, a
     )
         .title("Discord X NoRiskClient")
         .always_on_top(true)
-        .inner_size(1000.0, 900.0)
-        .resizable(false)
-        .minimizable(false)
         .center()
         .build()?;
 
