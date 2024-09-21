dabfb4d67d791fb01c8caaf2ecf75c9a83dd5350
copyandexecute <58366920+copyandexecute@users.noreply.github.com>
2024-09-21 22:02:20 +0200

    fix: include other .html to vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        svelte()
-    ]
-})
+  plugins: [
+    svelte(),
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        index: "./index.html",
+        logs: "./logs.html",
+        crash: "./crash.html",
+      },
+    },
+  },
+});
