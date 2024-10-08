87394ae45380e58ad72aacfea4f8c341d5b42eab
pvhil <54856838+pvhil@users.noreply.github.com>
2024-10-08 22:41:52 +0200

    added features to crashlog

--- src/logs/Crash.svelte
+++ src/logs/Crash.svelte
@@ -34,17 +34,83 @@
       addNotification(error);
     });
   }
+
+  let searchQuery = "";
+  let filteredLogs = [];
+  let logLevel;
+  let logCounts = {};
+
+
+  let logLevels = {
+    debug: false,
+    info: false,
+    warn: false,
+    error: false,
+    fatal: false,
+  };
+
+  $: {
+    minecraftLogs.subscribe(logs => {
+      const isAnyLevelSelected = Object.values(logLevels).some(level => level);
+      filteredLogs = logs.filter(log => {
+        const logMatch = log.match(/\[(.*?)\]/g);
+        if (logMatch && logMatch.length > 1) {
+          logLevel = logMatch[1].split('/')[1].replace(']', '');
+        }
+        if (isAnyLevelSelected) {
+          return logLevels[logLevel.toLowerCase()] && log.toLowerCase().includes(searchQuery.toLowerCase());
+        } else {
+          return log.toLowerCase().includes(searchQuery.toLowerCase());
+        }
+      });
+    });
+
+    Object.keys(logLevels).forEach(level => {
+      logCounts[level] = $minecraftLogs.filter(log => {
+        const logMatch = log.match(/\[(.*?)\]/g);
+        if (logMatch && logMatch.length > 1) {
+          const logLevel = logMatch[1].split('/')[1].replace(']', '');
+          return logLevel.toLowerCase() === level;
+        }
+        return false;
+      }).length;
+    });
+  }
+
+  function highlightSearchQuery(text) {
+    if (searchQuery === "") {
+      return text;
+    }
+    const parts = text.split(new RegExp(`(${searchQuery})`, 'gi'));
+    return parts.map(part => part.toLowerCase() === searchQuery.toLowerCase() ? `<span style="background-color:yellow;">${part}</span>` : part).join('');
+  }
+
 </script>
+
 
 
 <div class="black-bar" data-tauri-drag-region>
   <h1 class="back-button">CRASHED :(</h1>
+  <input class="search-bar" type="text" bind:value={searchQuery} placeholder="Search logs...">
+  {#each Object.keys(logLevels) as level (level)}
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <!-- svelte-ignore a11y-label-has-associated-control -->
+    <p class="levels" class:loglevel-button-on={logLevels[level]}
+      class:green-text={logLevels[level]}
+      class:loglevel-button-off={!logLevels[level]}
+      class:red-text={!logLevels[level]}
+      on:click={() => logLevels[level] = !logLevels[level]}>
+      {level} ({logCounts[level]})
+    </p>
+  {/each}
 </div>
 <main class="content">
   <div class="logs-wrapper">
-    <VirtualList items={$minecraftLogs} let:item>
-      <LogMessage text={item} />
-    </VirtualList>
+    <div class="logs-wrapper" >
+      <VirtualList items={filteredLogs} let:item>
+        <LogMessage text={highlightSearchQuery(item)} />
+      </VirtualList>
+    </div>
   </div>
 </main>
 <div class="black-bar" data-tauri-drag-region>
@@ -57,57 +123,86 @@
 </div>
 
 <style>
-    .black-bar {
-        display: flex;
-        align-content: center;
-        justify-content: center;
-        align-items: center;
-        width: 100%;
-        height: 10vh;
-        background-color: #151515;
-    }
+    .search-bar {
+    margin-right: 20px;
+    padding: 10px;
+    border: 3px solid #ccc;
+    border-radius: 0; 
+    outline: none;
+    box-shadow: 0 0 10px rgba(0,0,0,0.2);
+  }
+  .levels {
+    margin-right: 18px;
+  }
 
-    .content {
-        height: 80vh;
-    }
+  .search-bar {
+    margin-right: 20px;
+  }
+  .black-bar {
+      display: flex;
+      align-content: center;
+      justify-content: center;
+      align-items: center;
+      width: 100%;
+      height: 10vh;
+      background-color: #151515;
+  }
 
-    .logs-wrapper {
-        height: 100%;
-        display: flex;
-        flex-direction: column;
-        gap: 1em;
-    }
+  .content {
+      height: 80vh;
+  }
 
-    .back-button {
-        transition: transform 0.3s;
-        position: absolute;
-        font-size: 20px;
-        color: #e8e8e8;
-        text-shadow: 2px 2px #7a7777;
-        font-family: 'Press Start 2P', serif;
-    }
+  .logs-wrapper {
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      gap: 1em;
+  }
 
-    .back-button:hover {
-        transform: scale(1.2);
-    }
+  .back-button {
+      transition: transform 0.3s;
+      position: absolute;
+      font-size: 20px;
+      color: #e8e8e8;
+      text-shadow: 2px 2px #7a7777;
+      font-family: 'Press Start 2P', serif;
+  }
 
-    .logs-button-wrapper {
-        display: flex;
-        justify-content: space-between;
-        padding: 1em;
-        gap: 2em;
-    }
+  .back-button:hover {
+      transform: scale(1.2);
+  }
 
-    .copy-button {
-        transition: 0.3s;
-        font-family: 'Press Start 2P', serif;
-        font-size: 17px;
-        color: var(--primary-color);
-        text-shadow: 2px 2px var(--primary-color-text-shadow);
-        cursor: pointer;
-    }
+  .logs-button-wrapper {
+      display: flex;
+      justify-content: space-between;
+      padding: 1em;
+      gap: 2em;
+  }
 
-    .copy-button:hover {
-        transform: scale(1.2);
-    }
+  .copy-button {
+      transition: 0.3s;
+      font-family: 'Press Start 2P', serif;
+      font-size: 17px;
+      color: var(--primary-color);
+      text-shadow: 2px 2px var(--primary-color-text-shadow);
+      cursor: pointer;
+  }
+
+  .copy-button:hover {
+      transform: scale(1.2);
+  }
+
+    .loglevel-button-on {
+      transition: 0.2s;
+      font-family: 'Press Start 2P', serif;
+      font-size: 11px;
+      cursor: pointer;
+  }
+
+  .loglevel-button-off {
+      transition: 0.2s;
+      font-family: 'Press Start 2P', serif;
+      font-size: 11px;
+      cursor: pointer;
+  }
 </style>
